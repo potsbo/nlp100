1f148ce60addcf66dc996e0f02d85c44eff8da32
Shimpei Otsubo <potsbo@gmail.com>
2017-06-24 03:16:13 +0900

    Fix 004

--- 004.py
+++ 004.py
@@ -3,10 +3,11 @@ firsts = [1, 5, 6, 7, 8, 9, 15, 16, 19]
 
 words = string.split()
 
-elements = []
+elements = {}
 for i, word in enumerate(words):
-    e = word[:1] if i + 1 in firsts else word[:2]
-    elements.append(e)
+    num = i + 1
+    e = word[:1] if num in firsts else word[:2]
+    elements[e] = num
 
 print(elements)
 
