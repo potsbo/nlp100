4bf3b4de0630edf0ece1772d1f36d80a8809cb81
Shimpei Otsubo <potsbo@gmail.com>
2017-06-24 22:45:59 +0900

    Add 014

---
+++ 014.py
@@ -0,0 +1,18 @@
+# 14. 先頭からN行を出力
+# 自然数Nをコマンドライン引数などの手段で受け取り，入力のうち先頭のN行だけを表示せよ．確認にはheadコマンドを用いよ．
+
+import sys
+
+if len(sys.argv) < 2:
+    print('Input a number.')
+    exit()
+
+n = int(sys.argv[1])
+if not n:
+    print('Input a number.')
+    exit()
+
+lines = [line for line in open('./data/hightemp.txt')][:n]
+output = ''.join(lines)
+
+print(output, end='')
