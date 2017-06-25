17c4e3dcc6af151115083f8af5c1b5d27776826f
Shimpei Otsubo <potsbo@gmail.com>
2017-06-25 23:08:26 +0900

    Add 027

---
+++ 027.py
@@ -0,0 +1,15 @@
+# 27. 内部リンクの除去
+# 26の処理に加えて，テンプレートの値からMediaWikiの内部リンクマークアップを除去し，テキストに変換せよ（参考: マークアップ早見表）．
+
+import re
+
+nlp26 = __import__('026')
+
+def run():
+    dic = nlp26.run()
+    for (k,v) in dic.items():
+        dic[k] = re.sub(r"\[\[(.+\|)?(.+?)\]\]", r'\2', v)
+    return dic
+
+if __name__ == '__main__':
+    print(run())
