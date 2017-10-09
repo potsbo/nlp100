2f5399f701707db74bb9c0d92751e97317ff48e3
Shimpei Otsubo <potsbo@gmail.com>
2017-10-09 20:33:52 +0900

    Add 038

---
+++ 038.py
@@ -0,0 +1,15 @@
+# 38. ヒストグラム
+# 単語の出現頻度のヒストグラム（横軸に出現頻度，縦軸に出現頻度をとる単語の種類数を棒グラフで表したもの）を描け．
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+nlp36 = __import__('036')
+
+def run():
+    x = np.array([w[1] for w in nlp36.word_counts()])
+    plt.hist(x)
+    plt.show()
+
+if __name__ == '__main__':
+    print(run())
