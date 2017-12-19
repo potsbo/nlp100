6a3a940d6261782e7523eb8c36922abaa271e314
Shimpei Otsubo <potsbo@gmail.com>
2017-12-19 11:57:35 +0900

    Use makefile

--- 012.py
+++ 012.py
@@ -8,5 +8,5 @@ for line in f:
 col1 = '\n'.join([line[0] for line in columns])
 col2 = '\n'.join([line[1] for line in columns])
 
-open('./result/col1.txt', 'w').write(col1)
-open('./result/col2.txt', 'w').write(col2)
+open('./data/col1.txt', 'w').write(col1)
+open('./data/col2.txt', 'w').write(col2)
--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
+data/col1.txt: data/hightemp.txt
+	python 012.py
+
+data/col2.txt: data/hightemp.txt
+	python 012.py
+
 data/jawiki-country.json.gz:
 	curl http://www.cl.ecei.tohoku.ac.jp/nlp100/data/jawiki-country.json.gz > ./data/jawiki-country.json.gz
 
