bfdefc6d76dfe084fee8e564d35c66eed20127ed
Shimpei Otsubo <potsbo@gmail.com>
2017-12-20 07:13:31 +0900

    Add 040.py

---
+++ 040.py
@@ -0,0 +1,29 @@
+# 形態素を表すクラスMorphを実装せよ．
+# このクラスは表層形（surface），基本形（base），品詞（pos），品詞細分類1（pos1）
+# をメンバ変数に持つこととする．
+# さらに，CaboChaの解析結果（neko.txt.cabocha）を読み込み，
+# 各文をMorphオブジェクトのリストとして表現し，3文目の形態素列を表示せよ． 
+
+nlp30 = __import__('030')
+
+class Morph:
+    def __init__(self, obj):
+        self.surface = obj['surface']
+        self.base    = obj['base']
+        self.pos     = obj['pos']
+        self.pos1    = obj['pos1']
+
+    def string(self):
+        attrs = [self.surface, self.base, self.pos, self.pos1]
+        return ','.join(attrs)
+
+
+def run():
+    words  = nlp30.sentences()[2]
+    morphs = [Morph(w) for w in words]
+    strs   = [m.string() for m in morphs]
+    print(strs)
+
+if __name__ == '__main__':
+    run()
+
